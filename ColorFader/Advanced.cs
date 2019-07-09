8a71b90cce5ba8fc0a8e323fcd9b403c97d85433
ThirtysixLab <52709975+ThirtysixLab@users.noreply.github.com>
2019-07-09 18:06:28 +0300

    Add files via upload

---
+++ ColorFader/Advanced.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Advanced  {
+		//
+		// *** values out of range (0<->1), means that will used init render.color values  ***
+		//
+		public static void FadeIn(this GameObject go,float time = 1f, float delay = 0f){ 
+			if(go.GetComponent<ColorChanger>() == null) go.AddComponent<ColorChanger>().changeColor(new Color(2f,2f,2f,0f), new Color(2f,2f,2f,2f), time, delay);
+			else go.GetComponent<ColorChanger>().changeColor(new Color(2f,2f,2f,0f), new Color(2f,2f,2f,2f), time, delay);
+		}
+		//
+		public static void FadeOut(this GameObject go,float time = 1f, float delay = 0f){ 
+			if(go.GetComponent<ColorChanger>() == null) go.AddComponent<ColorChanger>().changeColor(new Color(2f,2f,2f,2f), new Color(2f,2f,2f,0f), time, delay);
+			else go.GetComponent<ColorChanger>().changeColor(new Color(2f,2f,2f,2f), new Color(2f,2f,2f,0f), time, delay);
+		}
+		//
+		public static void FadeColor(this GameObject go, Color col1 = default(Color), Color col2 = default(Color), float time = 1f, float delay = 0f){ 
+			if(go.GetComponent<ColorChanger>() == null) go.AddComponent<ColorChanger>().changeColor(col1, col2, time, delay);
+			else go.GetComponent<ColorChanger>().changeColor(col1, col2, time, delay);
+		}
+		//
+}
