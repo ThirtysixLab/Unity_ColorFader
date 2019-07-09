63606344c8e28902ec8fcc120d02412952742039
ThirtysixLab <52709975+ThirtysixLab@users.noreply.github.com>
2019-07-09 18:04:02 +0300

    Add files via upload

---
+++ Test_Action.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test_Action : MonoBehaviour
+{
+	public Color color1, color2;
+	public float duration1, delay1, duration2, delay2;
+	public bool repeat;
+	//
+    void Start()
+    {
+       if(repeat) InvokeRepeating("someAct",0f, duration1 + delay1 + duration2 + delay2);
+	   else someAct();
+    }
+	
+	void someAct(){
+		gameObject.FadeColor(color1, color2, duration1, delay1);
+		gameObject.FadeColor(color2, color1, duration2, delay2);
+		//gameObject.FadeIn(1,1);//duration, delay
+		//gameObject.FadeOut();// with default values duration = 1, delay = 0
+	}
+}
